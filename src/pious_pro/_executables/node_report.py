f9034332e510357303f96d0dfeaf99a48aa5549d
bkushigian <bkushigian@gmail.com>
2025-06-16 11:29:59 -0700

    Unused imports

--- src/pious_pro/_executables/node_report.py
+++ src/pious_pro/_executables/node_report.py
@@ -2,10 +2,6 @@ from typing import List
 from ..cli import CliSubcommand
 from argparse import _SubParsersAction
 import pickle
-import numpy as np
-import matplotlib.pyplot as plt
-from pious.util import PIO_HAND_ORDER
-from pious.util import color_card
 from ..mutate import NodeMutationData
 from ..node_report.node_report import NodeReport
 
